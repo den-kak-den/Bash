4d3ec54ba0e3d0573ecbb20e41efbe26d7358443
den-kak-den <82462614+den-kak-den@users.noreply.github.com>
2025-05-22 12:37:04 +0300

    Add some comments

--- 1stVersCheckScript.sh
+++ 1stVersCheckScript.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-filename="testfile.mp4"
+filename="testfile.mp4" 
 
-if [ ! -f "$filename" ]; then
+if [ ! -f "$filename" ]; then       #Если файл не найден, сообщаем об этом
         echo "Файл не найден"
         exit 1
 fi
 
-filesize=$(stat -c %s "$filename")
+filesize=$(stat -c %s "$filename")  #Если файл найден, присвоить значение его размера переменной
 
 if [ "$filesize" -gt $((10 * 1024 * 1024 * 1024)) ]; then
         echo "Restart please"
